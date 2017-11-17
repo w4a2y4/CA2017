65304cbfd00d3daf0b576e174b829becc29dd85c
Chih-Wei Ning <b04902079@ntu.edu.tw>
2017-11-17 16:23:40 +0800

    SE'

---
+++ hw4/Sign_Extend.v
@@ -0,0 +1,13 @@
+module Sign_Extend 
+{
+    data_i,
+    data_o
+}
+
+input	[15:0]	data_i;
+output	[31:0]	data_o;
+
+assign data_o[31:16] = data_i[15] ? 16'HFFFF : 16'H0000;
+assign data_o[15:0] = data_i;
+
+endmodule
