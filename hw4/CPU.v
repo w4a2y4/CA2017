09393a3790b643d085e6785d97d00ef169d3a03d
Chih-Wei Ning <b04902079@ntu.edu.tw>
2017-11-17 15:57:16 +0800

    CPU datapath

--- hw4/CPU.v
+++ hw4/CPU.v
@@ -10,90 +10,89 @@ input               clk_i;
 input               rst_i;
 input               start_i;
 
-/*
-Control Control(
-    .Op_i       (),
-    .RegDst_o   (),
-    .ALUOp_o    (),
-    .ALUSrc_o   (),
-    .RegWrite_o ()
-);
-*/
+wire [31:0] inst_addr, inst;
 
-/*
-Adder Add_PC(
-    .data1_in   (),
-    .data2_in   (),
-    .data_o     ()
+
+Control Control(
+    .Op_i       (inst[31:26]),
+    .RegDst_o   (MUX_RegDst.select_i),
+    .ALUOp_o    (ALU_Control.ALUOp_i),
+    .ALUSrc_o   (MUX_ALUSrc.select_i),
+    .RegWrite_o (Registers.RegWrite_o)
 );
-*/
+
+
+Adder Add_PC(
+    .data1_in   (inst_addr),
+    .data2_in   (32'd4),
+    .data_o     (PC.pc_i)
+);
+
 
 PC PC(
-    .clk_i      (),
-    .rst_i      (),
-    .start_i    (),
-    .pc_i       (),
-    .pc_o       ()
+    .clk_i      (clk_i),
+    .rst_i      (rst_i),
+    .start_i    (start_i),
+    .pc_i       (Add_PC.data_o),
+    .pc_o       (inst_addr)
 );
+
 
 Instruction_Memory Instruction_Memory(
-    .addr_i     (), 
-    .instr_o    ()
+    .addr_i     (inst_addr), 
+    .instr_o    (inst)
 );
+
 
 Registers Registers(
-    .clk_i      (),
-    .RSaddr_i   (),
-    .RTaddr_i   (),
-    .RDaddr_i   (), 
-    .RDdata_i   (),
-    .RegWrite_i (), 
-    .RSdata_o   (), 
-    .RTdata_o   () 
+    .clk_i      (clk_i),
+    .RSaddr_i   (inst[25:21]),
+    .RTaddr_i   (inst[20:16]),
+    .RDaddr_i   (MUX_RegDst.data_o), 
+    .RDdata_i   (ALU.data_o),
+    .RegWrite_i (Control.RegWrite_o), 
+    .RSdata_o   (ALU.data1_i), 
+    .RTdata_o   (MUX_ALUSrc.data1_i) 
 );
 
-/*
+
 MUX5 MUX_RegDst(
-    .data1_i    (),
-    .data2_i    (),
-    .select_i   (),
-    .data_o     ()
+    .data1_i    (inst[20:16]),
+    .data2_i    (inst[15:11]),
+    .select_i   (Control.RegDst_o),
+    .data_o     (Registers.RDaddr_i)
 );
-*/
 
-/*
+
 MUX32 MUX_ALUSrc(
-    .data1_i    (),
-    .data2_i    (),
-    .select_i   (),
-    .data_o     ()
+    .data1_i    (Registers.RTdata_o),
+    .data2_i    (Sign_Extend.data_o),
+    .select_i   (Control.ALUSrc_o),
+    .data_o     (ALU.data2_i)
 );
-*/
 
-/*
+
 Sign_Extend Sign_Extend(
-    .data_i     (),
-    .data_o     ()
+    .data_i     (inst[15:0]),
+    .data_o     (MUX_ALUSrc.data2_i)
 );
-*/
+
   
-/*
 ALU ALU(
-    .data1_i    (),
-    .data2_i    (),
-    .ALUCtrl_i  (),
-    .data_o     (),
+    .data1_i    (Registers.RSdata_o),
+    .data2_i    (MUX_ALUSrc.data_o),
+    .ALUCtrl_i  (ALU_Control.ALUCtrl_o),
+    .data_o     (Registers.RDdata_i),
     .Zero_o     ()
 );
-*/
 
-/*
+
 ALU_Control ALU_Control(
-    .funct_i    (),
-    .ALUOp_i    (),
-    .ALUCtrl_o  ()
+    .funct_i    (inst[5:0]),
+    .ALUOp_i    (Control.ALUOp_o),
+    .ALUCtrl_o  (ALU.ALUCtrl_i)
 );
-*/
+
 
 endmodule
 
